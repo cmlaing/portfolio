fcde15da9d9f3d2e3d34524f0aabf046594db2f6
Crystal Laing <cistle@gmail.com>
2021-07-14 14:41:59 -0700

    JS: Slide
    Slide JS added

---
+++ js/app.js
@@ -0,0 +1,13 @@
+/*==================================================================
+    
+    app.js
+    1. Includes JS plugin for sliding content on screen, taken 
+    from: https://github.com/mciastek/sal
+    Execute in this file by running: sal();
+    and including sal.js, sal.css
+
+==================================================================*/
+
+
+/* slide animation for each section */
+sal();
